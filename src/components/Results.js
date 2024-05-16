abc94a4bc0f6228a0f18ac0b4f4421f7ace86ac7
Pascal George <tamunoemi@gmail.com>
2024-05-16 02:28:51 -0400

    include two new props in the results function to manage the display of results as a modal and to close the modal. Function results should return a bootstrap modal instead of the previous div element

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,44 +1,33 @@
 // In Results.js
 import React from "react"; // Importing useRef
-import { Col, Row } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import CostDetails from "./CostDetails";
 import Affordability from "./Affordability";
-import { useFormContext } from "../context/FormContext";
 import "./Results.css";
+import { useFormContext } from "../context/FormContext";
 
-function Results({ costDetailsRef, affordabilityRef }) {
+function Results({ showResults, setShowResults }) {
   const { results } = useFormContext();
-  const canAfford = results && results.canAfford;
 
   return (
-    <Row className="mt-4 justify-content-center">
-      {results && (
-        <>
-          <Col
-            xs={12}
-            md={8}
-            lg={6}
-            className={`result-box ${
-              canAfford ? "affordable" : "unaffordable"
-            }`}
-            ref={costDetailsRef}
-          >
+    <Modal show={showResults} onHide={() => setShowResults(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Results</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {results && (
+          <>
             <CostDetails />
-          </Col>
-          <Col
-            xs={12}
-            md={8}
-            lg={6}
-            className={`result-box ${
-              canAfford ? "affordable" : "unaffordable"
-            }`}
-            ref={affordabilityRef}
-          >
             <Affordability />
-          </Col>
-        </>
-      )}
-    </Row>
+          </>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShowResults(false)}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
