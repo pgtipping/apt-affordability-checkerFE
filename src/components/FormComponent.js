dc81fe518653e633eded0c7dc08e3973c0b2431e
Pascal George <tamunoemi@gmail.com>
2024-06-07 06:48:53 -0400

    consolidate form inputs and update css

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -9,17 +9,13 @@ import {
   Form,
 } from "react-bootstrap";
 import { useFormContext } from "../context/FormContext";
-import MovingCostInput from "./forms/MovingCostInput";
-import ImmediateCostInput from "./forms/ImmediateCostInput";
-import RentInput from "./forms/RentInput";
+import MovingAndSetupCostInput from "./forms/MovingAndSetupCostInput";
+import MonthlyLivingCostInput from "./forms/MonthlyLivingCostInput";
+import MonthlyRentInput from "./forms/RentInput";
 import SecurityDepositInput from "./forms/SecurityDepositInput";
-import UtilitiesInput from "./forms/UtilitiesInput";
-import AssociatedCostInput from "./forms/AssociatedCostInput";
-import NetHouseholdIncomeInput from "./forms/NetHouseholdIncomeInput";
+import TotalMonthlyIncomeInput from "./forms/NetHouseholdIncomeInput";
 import TotalSavingsInput from "./forms/TotalSavingsInput";
 import MonthsToEvaluateInput from "./forms/MonthsToEvaluateInput";
-import OtherMonthlyExpensesInput from "./forms/OtherMonthlyExpensesInput";
-import MonthlySavingsInput from "./forms/MonthlySavingsInput";
 import Results from "./Results";
 import FeedbackForm from "./FeedbackForm";
 import Footer from "./Footer"; // Import the Footer component
@@ -33,12 +29,19 @@ function FormComponent() {
     formError,
     showFeedbackForm,
     setShowFeedbackForm,
+    handleInputChange,
   } = useFormContext();
   const [results, setResults] = useState(null);
   const [showResults, setShowResults] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
   const [userAdjustedSecurityDeposit, setUserAdjustedSecurityDeposit] =
     useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark-mode");
+    }
+  }, [darkMode]);
 
   const feedbackTooltip = (
     <Tooltip id="feedback-tooltip">Leave us feedback</Tooltip>
@@ -65,48 +68,52 @@ function FormComponent() {
     }
   }, [formData.rent, userAdjustedSecurityDeposit, setFormData]);
 
-  const handleInputChange = (fieldName, value) => {
+  const handleInputChangeWrapper = (fieldName, value) => {
     if (fieldName === "securityDeposit") {
       setUserAdjustedSecurityDeposit(true);
     }
-    setFormData((prev) => ({
-      ...prev,
-      [fieldName]: value,
-    }));
+    handleInputChange(fieldName, value);
   };
 
   return (
     <div className={`page-container ${darkMode ? "dark-mode" : ""}`}>
-      <Container className="content-wrap mt-5">
+      <Container className="content-wrap mt-6">
         <Row className="justify-content-center mt-3">
           <Col>
-            <Form.Check
-              type="switch"
-              id="darkModeSwitch"
-              label="Dark Mode"
-              checked={darkMode}
-              onChange={toggleDarkMode}
-            />
+            <div className="d-flex align-items-center justify-content-center">
+              <img
+                src="/sunny.png"
+                alt="Light Mode"
+                style={{ width: "55px" }}
+              />
+              <Form.Check
+                type="switch"
+                id="darkModeSwitch"
+                checked={darkMode}
+                onChange={toggleDarkMode}
+              />
+              <img src="/moon.png" alt="Dark Mode" style={{ width: "50px" }} />
+            </div>
           </Col>
         </Row>
         <Row className="justify-content-center mt-5">
           <Col xs={12} md={10} lg={8}>
             <div className="blog-intro mb-4">
-              <h2>Welcome to the Apartment Cost Analyzer!</h2>
+              <h2>Welcome to the Apartment Affordability Analyzer!</h2>
               <p>
                 Finding a new apartment is an exciting journey, but figuring out
-                if you can afford it can be daunting. Our Apartment Cost
-                Analyzer helps you evaluate your ability to afford your new
-                apartment with ease. Simply input the rental amount and other
-                associated costs, and let our tool do the rest. Whether you're
-                calculating moving costs, utilities, or other monthly expenses,
-                our analyzer provides a comprehensive view of your financial
-                commitment. Let's get started and ensure your new home fits
-                comfortably within your budget!
+                if you can afford it can be daunting. Our Apartment
+                Affordability Analyzer helps you evaluate your ability to afford
+                your new apartment with ease. Simply input the rental amount and
+                other associated costs, and let our tool do the rest. Whether
+                you're calculating moving costs, utilities, or other monthly
+                expenses, our analyzer provides a comprehensive view of your
+                financial commitment. Let's get started and ensure your new home
+                fits comfortably within your budget!
               </p>
             </div>
             <div className="d-flex justify-content-between align-items-center mb-3">
-              <h1 className="m-0">Rent Affordability Analyzer</h1>
+              <h1 className="m-0">Apartment Affordability Analyzer</h1>
               <OverlayTrigger placement="left" overlay={feedbackTooltip}>
                 <Button
                   variant={darkMode ? "outline-light" : "outline-primary"}
@@ -118,17 +125,19 @@ function FormComponent() {
             </div>
             {showFeedbackForm && <FeedbackForm />}
             <form onSubmit={handleSubmit}>
-              <MovingCostInput />
-              <ImmediateCostInput />
-              <RentInput onInputChange={handleInputChange} />
-              <SecurityDepositInput onInputChange={handleInputChange} />
-              <UtilitiesInput />
-              <AssociatedCostInput />
-              <NetHouseholdIncomeInput />
-              <TotalSavingsInput />
-              <MonthsToEvaluateInput />
-              <OtherMonthlyExpensesInput />
-              <MonthlySavingsInput />
+              <MovingAndSetupCostInput
+                onInputChange={handleInputChangeWrapper}
+              />
+              <MonthlyLivingCostInput
+                onInputChange={handleInputChangeWrapper}
+              />
+              <MonthlyRentInput onInputChange={handleInputChangeWrapper} />
+              <SecurityDepositInput onInputChange={handleInputChangeWrapper} />
+              <TotalMonthlyIncomeInput
+                onInputChange={handleInputChangeWrapper}
+              />
+              <TotalSavingsInput onInputChange={handleInputChangeWrapper} />
+              <MonthsToEvaluateInput onInputChange={handleInputChangeWrapper} />
               <Button
                 type="submit"
                 className={`btn ${darkMode ? "btn-dark" : "btn-primary"}`}
