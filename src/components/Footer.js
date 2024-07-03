f23f03a37d39fcab9403891b63c51e9e4c04f526
Pascal George <tamunoemi@gmail.com>
2024-07-03 02:30:43 -0400

    add fontawesome packages and update social icon for twitter to X. add reddit username to footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import { FaTwitter, FaReddit } from "react-icons/fa";
+import { FaReddit } from "react-icons/fa";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -43,13 +43,23 @@ function Footer() {
             href="https://x.com/Pgtipping01"
             target="_blank"
             rel="noopener noreferrer"
+            title="X"
           >
-            <FaTwitter className="social-icon" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              width="24"
+              height="24"
+              className="social-icon"
+            >
+              <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+            </svg>
           </a>
           <a
-            href="https://www.reddit.com/user/YourUsername"
+            href="https://www.reddit.com/user/Old-Dust977/"
             target="_blank"
             rel="noopener noreferrer"
+            title="Reddit"
           >
             <FaReddit className="social-icon" />
           </a>
