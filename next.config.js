6f966bcecbe8f29768a94bb28c8f6ae402395553
Pascal George <tamunoemi@gmail.com>
2025-04-14 20:06:08 -0400

    revert: remove cache buster from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,2 @@
 // Minimal Next.js config to enable Vercel build
-module.exports = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? `/?cacheBuster=${Date.now()}` : "",
-};
+module.exports = {};
